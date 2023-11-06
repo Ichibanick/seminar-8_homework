1caf9e4e8a7ba0c131ca57ffcfe889ab5dd5ef63
Shikalov Nikita <muchik.33@yandex.ru>
2023-11-06 16:26:39 +0300

    done 54

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54. Задайте двусмерный массив. Напишите программу, которая
+//  упорядочит по убыванию все элементы каждой строки
+
+// System.Console.WriteLine("Введите количество строк");
+// int rows = int.Parse(Console.ReadLine());
+
+// System.Console.WriteLine("Введите количество столбцов");
+// int colums = int.Parse(Console.ReadLine());
+
+// int[,] matrix= new int[rows,colums];
+
+// void GetArray()
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i,j]= new Random().Next(0,10);
+//         }
+//     }
+// }
+
+// void PrintArray()
+// {
+//  for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i,j]+" ");
+//         }
+//         System.Console.WriteLine();
+//     }   
+// }
+
+// void SortArray()
+// {
+//  for (int i = 0; i < matrix.GetLength(0); i++)
+//  {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         for (int k = 0; k < matrix.GetLength(1); k++)
+//         {
+//         if(matrix[i,j]>matrix[i,k])
+//         {
+//         int MaxEll= matrix[i,j];
+//         matrix[i,j] = matrix[i,k];
+//         matrix[i,k]=MaxEll;
+//         }
+//         }
+//  }   
+// }
+// }
+
+// GetArray();
+// PrintArray();
+// SortArray();
+// System.Console.WriteLine();
+// PrintArray();
