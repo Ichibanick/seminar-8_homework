70861e9b1410ddaf7448a405432d5d30af4d93bc
Shikalov Nikita <muchik.33@yandex.ru>
2023-11-09 17:19:27 +0300

    done 56

--- Program.cs
+++ Program.cs
@@ -56,3 +56,67 @@
 // SortArray();
 // System.Console.WriteLine();
 // PrintArray();
+
+
+
+
+// задача 56.Задайте двумерный прямоугольный массив.Напишите программу, которая будет находить строку с наименьшой 
+// суммой элементов.
+
+System.Console.WriteLine("Введите количество строк");
+int rows = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите количество столбцов");
+int colums = int.Parse(Console.ReadLine());
+
+int[,] matrix= new int[rows,colums];
+int SumRow = 0;
+int Temp =0;
+int MaxRow = 1;
+void GetArray()
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j]= new Random().Next(0,10);
+        }
+    }
+}
+
+void PrintArray()
+{
+ for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j]+" ");
+        }
+        System.Console.WriteLine();
+    }   
+}
+
+void FindRows()
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            SumRow += matrix[i,j];
+        }
+    System.Console.Write(SumRow +" ");
+    if(Temp>SumRow)
+    {
+        MaxRow = i-1;
+    }
+    Temp=SumRow;
+    System.Console.Write(Temp+" ");
+    SumRow =0;
+    }
+    System.Console.WriteLine("Максимальная сумма элементов в строке:"+ MaxRow);
+}
+
+GetArray();
+PrintArray();
+System.Console.WriteLine();
+FindRows();
